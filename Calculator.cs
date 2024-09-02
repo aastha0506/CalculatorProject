a0023e91fcc66bc4567654beaf26161a7dc26b0b
Aastha Bajaj <aastha.bajaj@planview.com>
2024-09-02 09:57:34 +0530

    Initial commit with Calculator class and addition function

---
+++ Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pipeline_project
+{
+    // Calculator.cs
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Pipeline_project;
+
+Console.WriteLine("Hello, World!");
+Calculator calc = new Calculator();
+Console.WriteLine("Addition of 5 and 3: " + calc.Add(5, 3));
